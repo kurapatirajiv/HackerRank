3a005047da4f41cac12b22998a0d192c87493600
Rajiv <kurapatirajiv@gmail.com>
2016-03-09 23:16:43 -0600

    Classes Torsional Angle
    Final Version

--- ClassTorsionalAngle.py
+++ ClassTorsionalAngle.py
@@ -1,11 +1,27 @@
-A = map(int, input.split())
-B = map(int, input.split())
-C = map(int, input.split())
-D = map(int, input.split())
-AB = []
-BC = []
-cD = []
-# Combining two individual vectors
-AB = AB.append(",".join(p-q for p,q in zip(B, A)))
-BC = BC.append(",".join(p-q for p,q in zip(B, A)))
-CD = CD.append(",".join(p-q for p,q in zip(B, A)))
+# https://www.hackerrank.com/challenges/class-2-find-the-torsional-angle
+import math
+
+def cross(a, b):
+    return [a[1]*b[2]-a[2]*b[1], a[2]*b[0]-a[0]*b[2], a[0]*b[1]-a[1]*b[0]]
+
+def dot(a, b):
+    return sum([a[x]*b[x] for x in range(3)])
+#Square root of sum of squares of individual items |X||Y| means integral Multiplication 
+def mod(a):
+    return sum([a[x]**2 for x in range(3)])**0.5
+
+a, b, c, d = (list(map(float, input().split())) for _ in range(4))
+
+ab = list([b[x]-a[x] for x in range(3)])
+bc = list([c[x]-b[x] for x in range(3)])
+cd = list([d[x]-c[x] for x in range(3)])
+
+X = cross(ab, bc)
+Y = cross(bc, cd)
+
+cosPhi = dot(X,Y)/(mod(X)*mod(Y))
+#Inverse is taken because the actual formula contains a negative sign
+PhiRad = math.acos(cosPhi)
+PhiDeg = math.degrees(PhiRad)
+
+print(round(PhiDeg,2))
