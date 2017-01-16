96924fedb5b25f47b3cc8948bcff7111e59a88d7
Rajiv <kurapatirajiv@gmail.com>
2017-01-16 17:58:58 -0600

    Roman to Integer Conversion

---
+++ Algorithms/RomantoInt.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+public class RomantoInt {
+    public int romanToInt(String s) {
+     HashMap<String,Integer> hm = new HashMap<String,Integer>();
+     hm.put("M",1000);
+     hm.put("D",500);
+     hm.put("C",100);
+     hm.put("L",50);
+     hm.put("X",10);
+     hm.put("V",5);
+     hm.put("I",1);
+     
+     String[] splitInput = s.split("");
+     int preval = 0;
+     int result = 0;
+     for(int i=splitInput.length-1; i>=0;i--){
+         String key = splitInput[i];
+         if(hm.containsKey(key)){
+            if(hm.get(key) >= preval){
+                result = result + hm.get(key);
+                preval = hm.get(key);
+            } 
+            else{
+                result = result - hm.get(key);
+            }
+         }
+         else{
+             System.out.println("Symbol not valid"+key);
+         }
+     }
+     return result;
+    }
+}
